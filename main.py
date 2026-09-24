303f4ce15774b60891fbf8cf43d60e8d87bfba64
ParvinSalahov <salahovprvin9@gmail.com>
2026-09-24 14:42:46 +0400

    Update data.txt with another large  text and update main.py according to data.txt

--- main.py
+++ main.py
@@ -102,4 +102,4 @@ def ask_rag_with_sources(query: str):
         print(f"      Content snippet: {doc.page_content[:100]}...\n")
 
 if __name__ == "__main__":
-    ask_rag_with_sources("What is covered in Week 2 of the internship?")
+    ask_rag_with_sources("What is the difference between Accuracy and Precision and when should each be used?")
